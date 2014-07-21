f3ff84260b29743befaf374702314e00cf6d229b
Brock Allen <brockallen@gmail.com>
2014-07-21 17:27:24 -0400

    EF sample

---
+++ source/EntityFramework/SelfHost/Config/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.EntityFramework;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Host.Config;
+
+namespace SelfHost.Config
+{
+    class Factory
+    {
+        public static IdentityServerServiceFactory Configure(string connString)
+        {
+            var svcFactory = new ServiceFactory(connString);
+            svcFactory.ConfigureClients(Clients.Get());
+            svcFactory.ConfigureScopes(Scopes.Get());
+
+            var factory = new IdentityServerServiceFactory();
+
+            var userService = new Thinktecture.IdentityServer.Core.Services.InMemory.InMemoryUserService(Users.Get());
+            factory.UserService = Registration.RegisterFactory<IUserService>(() => userService);
+
+            factory.ScopeService = Registration.RegisterFactory<IScopeService>(() => svcFactory.CreateScopeService());
+            factory.ClientService = Registration.RegisterFactory<IClientService>(() => svcFactory.CreateClientService());
+            
+            factory.AuthorizationCodeStore = Registration.RegisterFactory<IAuthorizationCodeStore>(() => svcFactory.CreateAuthorizationCodeStore());
+            factory.TokenHandleStore = Registration.RegisterFactory<ITokenHandleStore>(() => svcFactory.CreateTokenHandleStore());
+            factory.ConsentService = Registration.RegisterFactory<IConsentService>(() => svcFactory.CreateConsentService());
+            factory.RefreshTokenStore = Registration.RegisterFactory<IRefreshTokenStore>(() => svcFactory.CreateRefreshTokenStore());
+
+            return factory;
+        }
+    }
+}
---
+++ source/EntityFramework/SelfHost/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Owin;
+using SelfHost.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Services;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var options = new IdentityServerOptions
+            {
+                IssuerUri = "https://idsrv3.com",
+                SiteName = "Thinktecture IdentityServer v3 - preview 1 (EntityFramework)",
+                PublicHostName = "http://localhost:3333",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = Factory.Configure("IdSvr3Config"),
+                CorsPolicy = CorsPolicy.AllowAll
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
