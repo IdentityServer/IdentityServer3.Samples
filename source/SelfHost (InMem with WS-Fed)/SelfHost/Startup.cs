22c0616044ae4da31ff47214cae50df82dac713e
Dominick Baier <dbaier@leastprivilege.com>
2014-07-21 17:50:14 +0200

    Updated SelfHost sample

---
+++ source/SelfHost (InMem with WS-Fed)/SelfHost/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Owin;
+using SelfHost.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Host.Config;
+using Thinktecture.IdentityServer.WsFederation.Configuration;
+using Thinktecture.IdentityServer.WsFederation.Services;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = InMemoryFactory.Create(
+                users:   Users.Get(), 
+                clients: Clients.Get(), 
+                scopes:  Scopes.Get());
+
+            var options = new IdentityServerOptions
+            {
+                IssuerUri = "https://idsrv3.com",
+                SiteName = "Thinktecture IdentityServer v3 - preview 1 (SelfHost)",
+                PublicHostName = "http://localhost:3333",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = factory,
+                ConfigurePlugins = ConfigurePlugins
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+
+        private void ConfigurePlugins(IAppBuilder pluginApp, IdentityServerOptions options)
+        {
+            var wsFedOptions = new WsFederationPluginOptions
+            {
+                IdentityServerOptions = options,
+                Factory = new WsFederationServiceFactory
+                {
+                    UserService = options.Factory.UserService,
+                    RelyingPartyService = Registration.RegisterFactory<IRelyingPartyService>(() => new InMemoryRelyingPartyService(RelyingParties.Get())),
+                    WsFederationSettings = Registration.RegisterFactory<WsFederationSettings>(() => new WsFedSettings())
+                }
+            };
+
+            pluginApp.UseWsFederationPlugin(wsFedOptions);
+        }
+    }
+}
