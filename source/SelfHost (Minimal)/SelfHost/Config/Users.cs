84432482d33a007cef615bdeacad9a31c9e681f3
Dominick Baier <dbaier@leastprivilege.com>
2014-07-21 18:06:17 +0200

    Added minimal self host sample

---
+++ source/SelfHost (Minimal)/SelfHost/Config/Users.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+
+namespace SelfHost.Config
+{
+    static class Users
+    {
+        public static IEnumerable<InMemoryUser> Get()
+        {
+            return new InMemoryUser[]
+            {
+                new InMemoryUser{Subject = "alice", Username = "alice", Password = "alice", 
+                    Claims = new Claim[]
+                    {
+                        new Claim(Constants.ClaimTypes.GivenName, "Alice"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
+                        new Claim(Constants.ClaimTypes.Email, "AliceSmith@email.com"),
+                    }
+                },
+                new InMemoryUser{Subject = "bob", Username = "bob", Password = "bob", 
+                    Claims = new Claim[]
+                    {
+                        new Claim(Constants.ClaimTypes.GivenName, "Bob"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
+                        new Claim(Constants.ClaimTypes.Email, "BobSmith@email.com"),
+                    }
+                },
+            };
+        }
+    }
+}
