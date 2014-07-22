1f21fe0c6981371d0522c6b6f121889a499d9add
Brock Allen <brockallen@gmail.com>
2014-07-22 12:20:12 -0400

    MR sample

---
+++ source/UserService-MembershipReboot/SelfHost/Config/Factory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+using Thinktecture.IdentityServer.Host.Config;
+
+namespace SelfHost.Config
+{
+    class Factory
+    {
+        public static IdentityServerServiceFactory Configure(string connString)
+        {
+            var factory = new IdentityServerServiceFactory();
+
+            factory.UserService = Registration<IUserService>.RegisterFactory(()=>MembershipRebootUserServiceFactory.Factory(connString));
+
+            var scopeSvc = new InMemoryScopeService(Scopes.Get());
+            factory.ScopeService = Registration.RegisterFactory<IScopeService>(() => scopeSvc);
+            var clientSvc = new InMemoryClientService(Clients.Get());
+            factory.ClientService = Registration.RegisterFactory<IClientService>(() => clientSvc);
+
+            return factory;
+        }
+    }
+}
---
+++ source/UserService-MembershipReboot/SelfHost/MembershipRebootUserServiceFactory.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Ef;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.MembershipReboot;
+
+namespace SelfHost
+{
+    public class MembershipRebootUserServiceFactory
+    {
+        public static IUserService Factory(string connString)
+        {
+            var repo = new DefaultUserAccountRepository(connString);
+            var userAccountService = new UserAccountService(config, repo);
+            var userSvc = new UserService<UserAccount>(userAccountService, repo);
+            return userSvc;
+        }
+
+        static MembershipRebootConfiguration config;
+        static MembershipRebootUserServiceFactory()
+        {
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.MigrateDatabaseToLatestVersion<DefaultMembershipRebootDatabase, BrockAllen.MembershipReboot.Ef.Migrations.Configuration>());
+
+            config = new MembershipRebootConfiguration();
+            config.PasswordHashingIterationCount = 50000;
+            config.AllowLoginAfterAccountCreation = true;
+            config.RequireAccountVerification = false;
+        }
+    }
+}
