50548869ce03fefa9681614b3449991fb5300cb8
Brock Allen <brockallen@gmail.com>
2014-07-22 12:39:16 -0400

    aspnet identity sample

---
+++ source/UserService-AspNetIdentity/SelfHost/AspNetIdentityUserServiceFactory.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Thinktecture.IdentityServer.AspNetIdentity;
+using Thinktecture.IdentityServer.Core.Services;
+
+namespace SelfHost
+{
+    public class AspNetIdentityUserServiceFactory
+    {
+        static AspNetIdentityUserServiceFactory()
+        {
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.CreateDatabaseIfNotExists<IdentityDbContext>());
+            //System.Data.Entity.Database.SetInitializer(new System.Data.Entity.CreateDatabaseIfNotExists<CustomDbContext>());
+        }
+        
+        public static IUserService Factory(string connString)
+        {
+            var db = new IdentityDbContext<IdentityUser>(connString);
+            var store = new UserStore<IdentityUser>(db);
+            var mgr = new UserManager<IdentityUser>(store);
+            var userSvc = new UserService<IdentityUser, string>(mgr, db);
+            return userSvc;
+
+            //var db = new CustomDbContext("CustomAspId");
+            //var store = new CustomUserStore(db);
+            //var mgr = new CustomUserManager(store);
+            //var userSvc = new UserService<CustomUser, int>(mgr, db);
+            //return userSvc;
+        }
+    }
+}
---
+++ source/UserService-AspNetIdentity/SelfHost/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using SelfHost.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var options = new IdentityServerOptions
+            {
+                IssuerUri = "https://idsrv3.com",
+                SiteName = "Thinktecture IdentityServer v3 - UserService-AspNetIdentity",
+                PublicHostName = "http://localhost:3333",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = Factory.Configure("AspId"),
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
