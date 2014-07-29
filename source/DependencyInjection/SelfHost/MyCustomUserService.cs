41e30001cbf426eddebcc4843e4117bb1d87858e
Brock Allen <brockallen@gmail.com>
2014-07-29 18:46:29 -0400

    dependency injection sample

---
+++ source/DependencyInjection/SelfHost/MyCustomUserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Thinktecture.IdentityServer.Core.Services;
+
+namespace SelfHost
+{
+    public interface ICustomLogger
+    {
+        void Log(string message);
+    }
+
+    public class MyCustomDebugLogger : ICustomLogger
+    {
+        public void Log(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+
+    public class MyCustomTokenSigningService: ITokenSigningService
+    {
+        Thinktecture.IdentityServer.Core.Configuration.IdentityServerOptions options;
+        ICustomLogger logger;
+
+        public MyCustomTokenSigningService(
+            Thinktecture.IdentityServer.Core.Configuration.IdentityServerOptions options,
+            ICustomLogger logger)
+        {
+            this.options = options;
+            this.logger = logger;
+        }
+
+        public System.Threading.Tasks.Task<string> SignTokenAsync(Thinktecture.IdentityServer.Core.Connect.Models.Token token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/DependencyInjection/SelfHost/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Owin;
+using SelfHost.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Host.Config;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = InMemoryFactory.Create(
+                users : Users.Get(),
+                clients: Clients.Get(), 
+                scopes:  Scopes.Get());
+
+            //var factory = new IdentityServerServiceFactory();
+            factory.TokenSigningService = Registration.RegisterType<ITokenSigningService>(typeof(MyCustomTokenSigningService));
+            factory.Register(Registration.RegisterType<ICustomLogger>(typeof(MyCustomDebugLogger)));
+
+            var options = new IdentityServerOptions
+            {
+                IssuerUri = "https://idsrv3.com",
+                SiteName = "Thinktecture IdentityServer v3 - DependencyInjection",
+                PublicHostName = "http://localhost:3333",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = factory,
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
