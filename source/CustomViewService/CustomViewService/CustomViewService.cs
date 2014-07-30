16aaaacdc914018b3b56d1106a18cae44da15fda
Brock Allen <brockallen@gmail.com>
2014-07-30 13:27:12 -0400

    custom view service sample

---
+++ source/CustomViewService/CustomViewService/CustomViewService.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Thinktecture.IdentityServer.Core.Views;
+
+namespace SampleApp
+{
+    public class CustomViewService : IViewService
+    {
+        public virtual Task<System.IO.Stream> Login(IDictionary<string, object> env, LoginViewModel model)
+        {
+            return Render(model, "login");
+        }
+
+        public virtual Task<System.IO.Stream> Logout(IDictionary<string, object> env, LogoutViewModel model)
+        {
+            return Render(model, "logout");
+        }
+
+        public virtual Task<System.IO.Stream> LoggedOut(IDictionary<string, object> env, LoggedOutViewModel model)
+        {
+            return Render(model, "loggedOut");
+        }
+
+        public virtual Task<System.IO.Stream> Consent(IDictionary<string, object> env, ConsentViewModel model)
+        {
+            return Render(model, "consent");
+        }
+
+        public virtual Task<System.IO.Stream> Error(IDictionary<string, object> env, ErrorViewModel model)
+        {
+            return Render(model, "error");
+        }
+
+        protected virtual Task<System.IO.Stream> Render(CommonViewModel model, string page)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(model, Newtonsoft.Json.Formatting.None, new Newtonsoft.Json.JsonSerializerSettings() { ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver() });
+
+            string html = LoadHtml(page);
+            html = Replace(html, new {
+                siteName = model.SiteName,
+                model = json,
+            });
+            
+            return Task.FromResult(StringToStream(html));
+        }
+
+        private string LoadHtml(string name)
+        {
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"content\app");
+            file = Path.Combine(file, name + ".html");
+            return File.ReadAllText(file);
+        }
+
+        string Replace(string value, IDictionary<string, object> values)
+        {
+            foreach (var key in values.Keys)
+            {
+                value = value.Replace("{" + key + "}", values[key].ToString());
+            }
+            return value;
+        }
+
+        string Replace(string value, object values)
+        {
+            return Replace(value, Map(values));
+        }
+
+        IDictionary<string, object> Map(object values)
+        {
+            var dictionary = values as IDictionary<string, object>;
+
+            if (dictionary == null)
+            {
+                dictionary = new Dictionary<string, object>();
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(values))
+                {
+                    dictionary.Add(descriptor.Name, descriptor.GetValue(values));
+                }
+            }
+
+            return dictionary;
+        }
+        
+        Stream StringToStream(string s)
+        {
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms);
+            sw.Write(s);
+            sw.Flush();
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+    }
+}
---
+++ source/CustomViewService/CustomViewService/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Twitter;
+using Owin;
+using SampleApp.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Core.Views;
+using Thinktecture.IdentityServer.Core.Views.Embedded;
+
+namespace SampleApp
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/core", coreApp =>
+            {
+                var factory = InMemoryFactory.Create(
+                    users : Users.Get(),
+                    clients: Clients.Get(),
+                    scopes: Scopes.Get());
+
+                factory.ViewService = Registration.RegisterType<IViewService>(typeof(CustomViewService));
+
+                var options = new IdentityServerOptions
+                {
+                    IssuerUri = "https://idsrv3.com",
+                    SiteName = "Thinktecture IdentityServer v3 - CustomViewService",
+                    PublicHostName = "http://localhost:3333",
+                    SigningCertificate = Certificate.Get(),
+                    Factory = factory,
+                    AdditionalIdentityProviderConfiguration = ConfigureAdditionalIdentityProviders,
+                    CorsPolicy = CorsPolicy.AllowAll,
+                };
+
+                coreApp.UseIdentityServer(options);
+            });
+        }
+
+        public static void ConfigureAdditionalIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            var google = new GoogleAuthenticationOptions
+            {
+                AuthenticationType = "Google",
+                SignInAsAuthenticationType = signInAsType
+            };
+            app.UseGoogleAuthentication(google);
+
+            var fb = new FacebookAuthenticationOptions
+            {
+                AuthenticationType = "Facebook",
+                SignInAsAuthenticationType = signInAsType,
+                AppId = "676607329068058",
+                AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
+            };
+            app.UseFacebookAuthentication(fb);
+
+            var twitter = new TwitterAuthenticationOptions
+            {
+                AuthenticationType = "Twitter",
+                SignInAsAuthenticationType = signInAsType,
+                ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
+                ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
+            };
+            app.UseTwitterAuthentication(twitter);
+        }
+    }
+}
