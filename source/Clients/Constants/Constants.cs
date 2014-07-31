7268bda8eac1e2a72dc72a01a133f3b982ef64fd
Dominick Baier <dbaier@leastprivilege.com>
2014-07-31 14:46:12 +0200

    updated endsession URL

--- source/Clients/Constants/Constants.cs
+++ source/Clients/Constants/Constants.cs
@@ -6,7 +6,7 @@
         //public const string BaseAddress = "http://idsrv3.azurewebsites.net/core";
         
         public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
-        public const string LogoutEndpoint = BaseAddress + "/connect/logout";
+        public const string LogoutEndpoint = BaseAddress + "/connect/endsession";
         public const string TokenEndpoint = BaseAddress + "/connect/token";
 
         public const string AspNetWebApiSampleApi = "http://localhost:2727/";
