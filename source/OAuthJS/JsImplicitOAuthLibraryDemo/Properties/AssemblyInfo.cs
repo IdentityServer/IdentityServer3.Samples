695f44bf9a070ee62f04ac3eb728b527eeb6402d
Dominick Baier <dbaier@leastprivilege.com>
2014-09-25 15:36:11 +0200

    Added OAuth JS sample

---
+++ source/OAuthJS/JsImplicitOAuthLibraryDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsImplicitOAuthLibraryDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsImplicitOAuthLibraryDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9491f326-dbbe-4e20-aea0-91f6d80b114d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source/OAuthJS/JsImplicitOAuthLibraryDemo/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web.Http;
+using Thinktecture.IdentityModel.Tokens;
+
+namespace JsImplicitOAuthLibraryDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = ClaimMappings.None;
+            
+            var cert = Thinktecture.IdentityModel.X509.LocalMachine.TrustedPeople.SubjectDistinguishedName.Find("CN=idsrv3test", false).First();
+            app.UseJsonWebToken(
+                "https://idsrv3.com",
+                "https://idsrv3.com/resources",
+                cert);
+
+            var config = new HttpConfiguration();
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter("Bearer"));
+            config.Filters.Add(new AuthorizeAttribute());
+
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.Remove(config.Formatters.FormUrlEncodedFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            app.UseWebApi(config);
+        }
+    }
+}
