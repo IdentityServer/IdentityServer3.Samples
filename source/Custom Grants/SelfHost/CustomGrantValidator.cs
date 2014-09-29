a9948c44cb78cfa556f50551e57531a2e3a66140
Dominick Baier <dbaier@leastprivilege.com>
2014-09-29 20:42:15 +0200

    Updated custom grants sample (simple custom grants)

--- source/Custom Grants/SelfHost/CustomGrantValidator.cs
+++ source/Custom Grants/SelfHost/CustomGrantValidator.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
-using Thinktecture.IdentityModel;
 using Thinktecture.IdentityServer.Core.Connect;
 using Thinktecture.IdentityServer.Core.Plumbing;
 using Thinktecture.IdentityServer.Core.Services;
@@ -38,7 +33,7 @@ namespace SelfHost
                 "bob",
                 "bob",
                 "custom_grant",
-                "idsrv3");
+                "idsrv");
 
             return Task.FromResult(principal);
         }
