4065c4d18d18c9254bcc5c9c23bc371b7f4123d1
Dominick Baier <dbaier@leastprivilege.com>
2014-10-12 05:51:30 +0200

    Added Getting started sample (for tutorial)

---
+++ source/GettingStartedSample/EmbeddedMvc/IdentityServer/Users.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+
+namespace EmbeddedMvc.IdentityServer
+{
+public static class Users
+{
+    public static IEnumerable<InMemoryUser> Get()
+    {
+        return new[]
+        {
+            new InMemoryUser
+            {
+                Username = "bob",
+                Password = "secret",
+                Subject = "1",
+
+                Claims = new[]
+                {
+                    new Claim(Constants.ClaimTypes.GivenName, "Bob"),
+                    new Claim(Constants.ClaimTypes.FamilyName, "Smith")
+                }
+            }
+        };
+    }
+}
+}
