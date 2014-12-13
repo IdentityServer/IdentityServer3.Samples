85873640447854cf926d4f8c5f2ab0d6b79c42fa
Dominick Baier <dbaier@leastprivilege.com>
2014-12-13 08:50:06 +0100

    Added self host sample that uses Serilog

---
+++ source/SelfHost (Minimal with Serilog)/SelfHost/Config/Scopes.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace Thinktecture.IdentityServer.Host.Config
+{
+    public class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new[]
+                {
+                    ////////////////////////
+                    // identity scopes
+                    ////////////////////////
+
+                    StandardScopes.OpenId,
+                    StandardScopes.Profile,
+                    StandardScopes.Email,
+                    StandardScopes.Address,
+                    StandardScopes.OfflineAccess,
+                    StandardScopes.RolesAlwaysInclude,
+                    StandardScopes.AllClaims,
+
+                    ////////////////////////
+                    // resource scopes
+                    ////////////////////////
+
+                    new Scope
+                    {
+                        Name = "read",
+                        DisplayName = "Read data",
+                        Type = ScopeType.Resource,
+                        Emphasize = false,
+                    },
+                    new Scope
+                    {
+                        Name = "write",
+                        DisplayName = "Write data",
+                        Type = ScopeType.Resource,
+                        Emphasize = true,
+                    },
+                    new Scope
+                    {
+                        Name = "idmgr",
+                        DisplayName = "IdentityManager",
+                        Type = ScopeType.Resource,
+                        Emphasize = true,
+                        ShowInDiscoveryDocument = false,
+                        
+                        Claims = new[]
+                        {
+                            new ScopeClaim(Constants.ClaimTypes.Name),
+                            new ScopeClaim(Constants.ClaimTypes.Role)
+                        }
+                    }
+                };
+        }
+    }
+}
