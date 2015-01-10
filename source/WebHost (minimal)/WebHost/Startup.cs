0917a1f74c5aed78285a5b87803a297e84429297
Dominick Baier <dbaier@leastprivilege.com>
2015-01-10 11:54:46 +0100

    Added minimal web host sample

---
+++ source/WebHost (minimal)/WebHost/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Configuration;
+using Thinktecture.IdentityServer.Core.Configuration;
+
+[assembly: OwinStartup(typeof(WebHost.Startup))]
+
+namespace WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = InMemoryFactory.Create(
+                users: Users.Get(),
+                clients: Clients.Get(),
+                scopes: Scopes.Get());
+
+            var options = new IdentityServerOptions
+            {
+                SigningCertificate = Certificate.Load(),
+                Factory = factory,
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
