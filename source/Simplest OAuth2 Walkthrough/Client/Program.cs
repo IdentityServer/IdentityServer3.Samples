6a390b27c983b71dde61bdc937a3c21f04daf930
fabriciomurta <fabricio-github@familiamurta.org>
2015-03-01 02:45:20 -0300

    Disable SSL validation for Client > IdentityServer

--- source/Simplest OAuth2 Walkthrough/Client/Program.cs
+++ source/Simplest OAuth2 Walkthrough/Client/Program.cs
@@ -8,6 +8,12 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            // Disable SSL Validation for testing purposes
+            // Credits: http://stackoverflow.com/questions/777607/the-remote-certificate-is-invalid-according-to-the-validation-procedure-using
+            ServicePointManager.ServerCertificateValidationCallback =
+                delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                { return true; };
+            
             var response = GetClientToken();
             CallApi(response);
 
