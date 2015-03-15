b082142a1a4019eb97f034f650ca7d3da9aec28b
Dominick Baier <dbaier@leastprivilege.com>
2015-03-15 17:15:22 +0100

    Added AspNet5 hosting sample

---
+++ source/AspNet5Host/src/IdentityServer3/Configuration/Users.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+
+namespace AspNet5Host.Configuration
+{
+    static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            var users = new List<InMemoryUser>
+            {
+                new InMemoryUser{Subject = "818727", Username = "alice", Password = "alice",
+                    Claims = new Claim[]
+                    {
+                        new Claim(Constants.ClaimTypes.Name, "Alice Smith"),
+                        new Claim(Constants.ClaimTypes.GivenName, "Alice"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
+                        new Claim(Constants.ClaimTypes.Email, "AliceSmith@email.com"),
+                        new Claim(Constants.ClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(Constants.ClaimTypes.Role, "Admin"),
+                        new Claim(Constants.ClaimTypes.Role, "Geek"),
+                        new Claim(Constants.ClaimTypes.WebSite, "http://alice.com"),
+                        new Claim(Constants.ClaimTypes.Address, "{ \"street_address\": \"One Hacker Way\", \"locality\": \"Heidelberg\", \"postal_code\": 69118, \"country\": \"Germany\" }")
+                    }
+                },
+                new InMemoryUser{Subject = "88421113", Username = "bob", Password = "bob",
+                    Claims = new Claim[]
+                    {
+                        new Claim(Constants.ClaimTypes.Name, "Bob Smith"),
+                        new Claim(Constants.ClaimTypes.GivenName, "Bob"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
+                        new Claim(Constants.ClaimTypes.Email, "BobSmith@email.com"),
+                        new Claim(Constants.ClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(Constants.ClaimTypes.Role, "Developer"),
+                        new Claim(Constants.ClaimTypes.Role, "Geek"),
+                        new Claim(Constants.ClaimTypes.WebSite, "http://bob.com"),
+                        new Claim(Constants.ClaimTypes.Address, "{ \"street_address\": \"One Hacker Way\", \"locality\": \"Heidelberg\", \"postal_code\": 69118, \"country\": \"Germany\" }")
+                    }
+                },
+            };
+
+            return users;
+        }
+    }
+}
