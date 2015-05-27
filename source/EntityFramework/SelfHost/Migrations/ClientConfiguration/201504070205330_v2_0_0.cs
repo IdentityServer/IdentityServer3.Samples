91b0e154af458c52053024b4d4609b3a24f175fa
Brock Allen <brockallen@gmail.com>
2015-05-27 10:42:52 -0400

    updated migration for column name changes

--- source/EntityFramework/SelfHost/Migrations/ClientConfiguration/201504070205330_v2_0_0.cs
+++ source/EntityFramework/SelfHost/Migrations/ClientConfiguration/201504070205330_v2_0_0.cs
@@ -12,12 +12,16 @@ namespace SelfHost.Migrations.ClientConfiguration
             AddColumn("dbo.Clients", "AllowAccessToAllScopes", c => c.Boolean(nullable: false));
             AddColumn("dbo.Clients", "AllowAccessToAllCustomGrantTypes", c => c.Boolean(nullable: false));
             RenameColumn("dbo.ClientSecrets", "ClientSecretType", "Type");
+            RenameColumn("dbo.Clients", "AllowAccessToAllCustomGrantTypes", "AllowAccessToAllGrantTypes");
+            RenameColumn("dbo.Clients", "UpdateAccessTokenClaimsOnRefresh", "UpdateAccessTokenOnRefresh");
             CreateIndex("dbo.Clients", "ClientId", unique: true);
         }
         
         public override void Down()
         {
             DropIndex("dbo.Clients", new[] { "ClientId" });
+            RenameColumn("dbo.Clients", "UpdateAccessTokenOnRefresh", "UpdateAccessTokenClaimsOnRefresh");
+            RenameColumn("dbo.Clients", "AllowAccessToAllGrantTypes", "AllowAccessToAllCustomGrantTypes");
             RenameColumn("dbo.ClientSecrets", "Type", "ClientSecretType");
             DropColumn("dbo.Clients", "AllowAccessToAllCustomGrantTypes");
             DropColumn("dbo.Clients", "AllowAccessToAllScopes");
