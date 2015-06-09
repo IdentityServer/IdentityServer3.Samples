c661f8097ea81b47d8ae690b8ad39dcb17857c12
Brock Allen <brockallen@gmail.com>
2015-06-09 14:26:46 -0400

    change namespace on many extension methods

---
+++ source/MembershipReboot/WebHost/IdSvr/Factory.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Copyright 2014 Dominick Baier, Brock Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services;
+using IdentityServer3.Core.Services.InMemory;
+using IdentityServer3.Core.Services.Default;
+
+namespace WebHost.IdSvr
+{
+    class Factory
+    {
+        public static IdentityServerServiceFactory Configure()
+        {
+            var factory = new IdentityServerServiceFactory();
+
+            var scopeStore = new InMemoryScopeStore(Scopes.Get());
+            factory.ScopeStore = new Registration<IScopeStore>(resolver => scopeStore);
+            
+            var clientStore = new InMemoryClientStore(Clients.Get());
+            factory.ClientStore = new Registration<IClientStore>(resolver => clientStore);
+
+            factory.CorsPolicyService = new Registration<ICorsPolicyService>(new DefaultCorsPolicyService { AllowAll = true });
+
+            return factory;
+        }
+    }
+}
---
+++ source/MembershipReboot/WebHost/Startup.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Twitter;
+/*
+ * Copyright 2014 Dominick Baier, Brock Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Owin;
+using WebHost.IdSvr;
+using IdentityManager.Configuration;
+using IdentityServer3.Core.Configuration;
+using WebHost.IdMgr;
+using IdentityManager.Core.Logging;
+using IdentityManager.Logging;
+using Serilog;
+
+namespace WebHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.Trace()
+               .CreateLogger(); 
+            
+            var connectionString = "MembershipReboot";
+
+            app.Map("/admin", adminApp =>
+            {
+                var factory = new IdentityManagerServiceFactory();
+                factory.Configure(connectionString);
+
+                adminApp.UseIdentityManager(new IdentityManagerOptions()
+                {
+                    Factory = factory
+                });
+            });
+
+            app.Map("/core", core =>
+            {
+                var idSvrFactory = Factory.Configure();
+                idSvrFactory.ConfigureCustomUserService(connectionString);
+
+                var options = new IdentityServerOptions
+                {
+                    SiteName = "IdentityServer3 - UserService-MembershipReboot",
+                
+                    SigningCertificate = Certificate.Get(),
+                    Factory = idSvrFactory,
+                    AuthenticationOptions = new AuthenticationOptions{
+                        IdentityProviders = ConfigureAdditionalIdentityProviders,
+                    }
+                };
+
+                core.UseIdentityServer(options);
+            });
+        }
+
+        public static void ConfigureAdditionalIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            var google = new GoogleOAuth2AuthenticationOptions
+            {
+                AuthenticationType = "Google",
+                SignInAsAuthenticationType = signInAsType,
+                ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
+                ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
+            };
+            app.UseGoogleAuthentication(google);
+
+            var fb = new FacebookAuthenticationOptions
+            {
+                AuthenticationType = "Facebook",
+                SignInAsAuthenticationType = signInAsType,
+                AppId = "676607329068058",
+                AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
+            };
+            app.UseFacebookAuthentication(fb);
+
+            var twitter = new TwitterAuthenticationOptions
+            {
+                AuthenticationType = "Twitter",
+                SignInAsAuthenticationType = signInAsType,
+                ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
+                ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
+            };
+            app.UseTwitterAuthentication(twitter);
+        }
+    }
+}
