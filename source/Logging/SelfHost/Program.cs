0e6aa3e1435a441cf26b3aab8cccc3d11b216946
Dominick Baier <dbaier@leastprivilege.com>
2015-09-01 17:08:51 +0200

    Added a sample for adding a custom logging sink and using ILog in custom services

---
+++ source/Logging/SelfHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using Serilog;
+using System;
+
+namespace SelfHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer3 SelfHost";
+
+            // log to both console and custom trace source sink
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.LiterateConsole(outputTemplate: "{Timestamp:HH:MM} [{Level}] ({Name:l}){NewLine} {Message}{NewLine}{Exception}")
+                .WriteTo.TraceSource(traceSourceName: "IdentityServer3")
+                .CreateLogger();
+
+            const string url = "https://localhost:44333/core";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ source/Logging/SelfHost/Startup.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services;
+using Owin;
+using SelfHost.Config;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryClients(Clients.Get())
+                .UseInMemoryScopes(Scopes.Get())
+                .UseInMemoryUsers(Users.Get());
+
+            factory.ClaimsProvider = 
+                new Registration<IClaimsProvider, CustomClaimsProvider>();
+
+            var options = new IdentityServerOptions
+            {
+                SiteName = "IdentityServer3 (self host)",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = factory,
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
