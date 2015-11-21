5ad595e07bd2b3ce94490692fea812db466fa1d2
Dominick Baier <dbaier@leastprivilege.com>
2015-11-21 15:12:25 +0100

    Deleted old aspnet5 sample - created a new one for RC1

---
+++ source/AspNet5/src/Api/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseIISPlatformHandler();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseJwtBearerAuthentication(options =>
+            {
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+
+                options.Audience = "http://localhost:5000/resources";
+                options.AutomaticAuthenticate = true;
+            });
+
+            app.UseMiddleware<RequiredScopesMiddleware>(new List<string> { "api1" });
+
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
---
+++ source/AspNet5/src/MvcClient/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            app.UseIISPlatformHandler();
+
+            app.UseCookieAuthentication(options =>
+            {
+                options.AuthenticationScheme = "Cookies";
+                options.AutomaticAuthenticate = true;
+            });
+
+            app.UseOpenIdConnectAuthentication(options =>
+            {
+                options.AutomaticChallenge = true;
+                options.AuthenticationScheme = "Oidc";
+                options.SignInScheme = "Cookies";
+
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+
+                options.ClientId = "mvc6";
+                options.ResponseType = "id_token token";
+
+                options.Scope.Add("openid");
+                options.Scope.Add("email");
+                options.Scope.Add("api1");
+            });
+
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
