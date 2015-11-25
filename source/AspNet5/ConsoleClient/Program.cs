0d37c4a966e2a6ebdc91272bc130ff602d0373fb
Dominick Baier <dbaier@leastprivilege.com>
2015-11-25 08:58:05 +0100

    added console client

---
+++ source/AspNet5/ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenClient = new TokenClient(
+                "http://localhost:5000/connect/token",
+                "test",
+                "secret");
+
+            var response = tokenClient.RequestClientCredentialsAsync("api1").Result;
+
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            var apiResponse = client.GetAsync("http://localhost:19806/identity").Result;
+            Console.WriteLine(apiResponse.StatusCode);
+        }
+    }
+}
