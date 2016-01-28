1b1d590e6593f4b487aaba46fbd99152d07215fe
Eran Stiller <git@stiller.co.il>
2016-01-28 23:11:58 +0200

    Add Windows Auth Sample

---
+++ source/WebHost (Windows Auth)/WebHost/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Configuration;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services;
+using Microsoft.Owin.Security.WsFederation;
+using Serilog;
+
+[assembly: OwinStartup(typeof(WebHost.Startup))]
+
+namespace WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Trace(outputTemplate: "{Timestamp} [{Level}] ({Name}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryClients(Clients.Get())
+                .UseInMemoryScopes(Scopes.Get());
+            factory.UserService = new Registration<IUserService>(typeof(ExternalRegistrationUserService));
+
+            var options = new IdentityServerOptions
+            {
+                SigningCertificate = Certificate.Load(),
+                Factory = factory,
+                AuthenticationOptions = new AuthenticationOptions
+                {
+                    EnableLocalLogin = false,
+                    IdentityProviders = ConfigureIdentityProviders
+                }
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+
+        private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            var wsFederation = new WsFederationAuthenticationOptions
+            {
+                AuthenticationType = "windows",
+                Caption = "Windows",
+                SignInAsAuthenticationType = signInAsType,
+
+                MetadataAddress = "https://localhost:44366",
+                Wtrealm = "urn:idsrv3"
+            };
+            app.UseWsFederationAuthentication(wsFederation);
+        }
+    }
+}
