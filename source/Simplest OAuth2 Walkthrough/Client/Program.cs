f82cb6d06753e58529caa877d4b213641c8f1907
Dominick Baier <dbaier@leastprivilege.com>
2016-01-31 09:12:01 +0100

    remove SSL from oauth2 walkthrough

--- source/Simplest OAuth2 Walkthrough/Client/Program.cs
+++ source/Simplest OAuth2 Walkthrough/Client/Program.cs
@@ -26,7 +26,7 @@ namespace Client
         static TokenResponse GetClientToken()
         {
             var client = new TokenClient(
-                "https://localhost:44333/connect/token",
+                "http://localhost:5000/connect/token",
                 "silicon",
                 "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
 
@@ -36,7 +36,7 @@ namespace Client
         static TokenResponse GetUserToken()
         {
             var client = new TokenClient(
-                "https://localhost:44333/connect/token",
+                "http://localhost:5000/connect/token",
                 "carbon",
                 "21B5F798-BE55-42BC-8AA8-0025B903DC3B");
 
