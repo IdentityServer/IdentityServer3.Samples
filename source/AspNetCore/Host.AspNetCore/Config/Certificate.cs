1a746e91750538cd2b162fd20bc72970eda1391b
Brock Allen <brockallen@gmail.com>
2016-08-15 09:44:06 -0400

    add aspnet core sample host

---
+++ source/AspNetCore/Host.AspNetCore/Config/Certificate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SampleApp.Config
+{
+    static class Certificate
+    {
+        internal static X509Certificate2 Get(string contentRootPath)
+        {
+            return new X509Certificate2(Path.Combine(contentRootPath, "Config\\idsrv3test.pfx"), "idsrv3test");
+        }
+    }
+}
---
+++ source/AspNetCore/Host.AspNetCore/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin.Builder;
+using Owin;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace Host.AspNetCore
+{
+    using IdentityServer3.Core.Configuration;
+    using IdentityServer3.Host.Config;
+    using Microsoft.AspNetCore.Hosting;
+    using SampleApp.Config;
+    using Serilog;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDataProtection();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment host)
+        {
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.File(@"c:\logs\idsvrlog.txt")
+               .CreateLogger();
+
+            app.Map("/core", coreApp =>
+            {
+                var factory = new IdentityServerServiceFactory()
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+                    .UseInMemoryUsers(Users.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    SiteName = "IdentityServer3 - AspNet Core",
+
+                    SigningCertificate = Certificate.Get(host.ContentRootPath),
+                    Factory = factory,
+                };
+
+                coreApp.UseIdentityServer(options);
+            });
+        }
+    }
+}
