4a233e923121042919a243eec66e0893d1a1f687
Luděk Rous <ludek-r@volny.cz>
2016-08-16 10:28:13 +0200

    Post logout redirect settings in MVC 6
    ASP.NET Core 1.0 alternative for https://github.com/IdentityServer/IdentityServer3.Samples/blob/master/source/Clients/MVC%20OWIN%20Client%20(Hybrid)/Startup.cs

--- source/AspNet5/src/MvcClient/Startup.cs
+++ source/AspNet5/src/MvcClient/Startup.cs
@@ -23,27 +23,139 @@ namespace MvcClient
                 options.AuthenticationScheme = "Cookies";
                 options.AutomaticAuthenticate = true;
             });
-
-            app.UseOpenIdConnectAuthentication(options =>
+            
+            var oidcOptions = new OpenIdConnectOptions
             {
-                options.AutomaticChallenge = true;
-                options.AuthenticationScheme = "Oidc";
-                options.SignInScheme = "Cookies";
+                AutomaticChallenge = true;
+                AuthenticationScheme = "Oidc";
+                SignInScheme = "Cookies";
 
-                options.Authority = "http://localhost:18942/";
-                options.RequireHttpsMetadata = false;
+                Authority = "http://localhost:18942/";
+                RequireHttpsMetadata = false;
 
-                options.ClientId = "mvc6";
-                options.ResponseType = "id_token token";
+                ClientId = "mvc6";
+                ResponseType = "id_token token";
+                PostLogoutRedirectUri = "http://localhost:19276/"; // Has to be in PostLogoutRedirectUris in client settings on identity server
+            };
 
-                options.Scope.Add("openid");
-                options.Scope.Add("email");
-                options.Scope.Add("api1");
-            });
+            oidcOptions.Scope.Clear();
+            oidcOptions.Scope.Add("openid");
+            oidcOptions.Scope.Add("profile");
+            oidcOptions.Scope.Add("email");
+            oidcOptions.Scope.Add("api1");
+
+
+            oidcOptions.Events = new OpenIdConnectEvents()
+            {                
+                OnRedirectToIdentityProvider = RedirectToIdentityProvider,
+                OnAuthorizationCodeReceived = AuthorizationCodeReceived,
+                OnTokenValidated = TokenValidated,
+                OnMessageReceived = MessageReceived,
+                OnAuthenticationFailed = AuthenticationFailed,
+                OnRedirectToIdentityProviderForSignOut = RedirectToIdentityProviderForSignOut,
+                OnRemoteSignOut = RemoteSignOut,
+                OnTicketReceived = TicketReceived,
+                OnTokenResponseReceived = TokenResponseReceived,
+                OnUserInformationReceived = UserInformationReceived
+                
+            };
+            
+            app.UseOpenIdConnectAuthentication(oidcOptions);
 
             app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
         }
+
+        #region Events
+        public async Task RedirectToIdentityProvider(RedirectContext rc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("RedirectToIdentityProvider");
+            });
+        }
+
+        public async Task AuthorizationCodeReceived(AuthorizationCodeReceivedContext acrc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("AuthorizationCodeReceived");
+            });
+        }
+
+        public async Task TokenValidated(TokenValidatedContext tvc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("TokenValidated");
+            });
+        }
+
+        public async Task MessageReceived(MessageReceivedContext mrc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("MessageReceived");
+            });
+        }
+
+        public async Task AuthenticationFailed(AuthenticationFailedContext afc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("AuthenticationFailed");
+            });
+        }
+
+        public async Task RemoteSignOut(RemoteSignOutContext rsoc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("RemoteSignOut");
+            });
+        }
+
+        public async Task RedirectToIdentityProviderForSignOut(RedirectContext rc)
+        {
+            await Task.Run(() =>
+            {
+                rc.ProtocolMessage.IdTokenHint = rc.HttpContext.User.FindFirst("id_token").Value;
+                Debug.WriteLine("RedirectToIdentityProviderForSignOut");
+            });
+        }
+
+        public async Task TicketReceived(TicketReceivedContext trc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("TicketReceived");
+            });
+        }
+
+        public async Task TokenResponseReceived(TokenResponseReceivedContext trrc)
+        {
+            await Task.Run(() =>
+            {
+                Debug.WriteLine("TokenResponseReceived");
+            });
+        }
+
+        public async Task UserInformationReceived(UserInformationReceivedContext uirc)
+        {
+            await Task.Run(() =>
+            {
+                var identity = uirc.Ticket.Principal.Identity as ClaimsIdentity;
+                identity.AddClaim(new Claim("access_token", uirc.ProtocolMessage.AccessToken));
+                identity.AddClaim(new Claim("expires_at", DateTime.Now.AddSeconds(Convert.ToDouble(uirc.ProtocolMessage.ExpiresIn)).ToLocalTime().ToString()));
+                //identity.Claims.Append(new Claim("refresh_token", uirc.ProtocolMessage.RefreshToken));
+                identity.AddClaim(new Claim("id_token", uirc.ProtocolMessage.IdToken));
+                identity.Claims.Append(new Claim("sid", uirc.Ticket.Principal.FindFirst("sid").Value));
+
+
+                Debug.WriteLine("UserInformationReceived");
+            });
+        }
+        #endregion
 
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
