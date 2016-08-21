5493287377dd7c7a251090673db6360273762c69
John10 Korsnes <johnkors@gmail.com>
2016-08-21 22:07:13 +0200

    Removed noise for sample

--- source/LocalizedMessages/LocalizedMessages/Startup.cs
+++ source/LocalizedMessages/LocalizedMessages/Startup.cs
@@ -6,10 +6,8 @@ using Microsoft.Owin.Security.Twitter;
 using Owin;
 using SampleApp.Config;
 using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core.Logging;
 using IdentityServer3.Core.Services;
 using IdentityServer3.Core.Services.Contrib;
-using IdentityServer3.Core.Services.Default;
 using Serilog;
 using IdentityServer3.Host.Config;
 using Microsoft.Owin;
@@ -56,44 +54,13 @@ namespace SampleApp
                     SiteName = "IdentityServer33 - Localized from accept-language http header Messages",
 
                     SigningCertificate = Certificate.Get(),
-                    Factory = factory,
-                    AuthenticationOptions = new AuthenticationOptions {
-                        IdentityProviders = ConfigureAdditionalIdentityProviders,
-                    }
+                    Factory = factory
                 };
 
                 coreApp.UseIdentityServer(options);
             });
         }
 
-        public static void ConfigureAdditionalIdentityProviders(IAppBuilder app, string signInAsType)
-        {
-            var google = new GoogleOAuth2AuthenticationOptions
-            {
-                AuthenticationType = "Google",
-                SignInAsAuthenticationType = signInAsType,
-                ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
-                ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
-            };
-            app.UseGoogleAuthentication(google);
-
-            var fb = new FacebookAuthenticationOptions
-            {
-                AuthenticationType = "Facebook",
-                SignInAsAuthenticationType = signInAsType,
-                AppId = "676607329068058",
-                AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
-            };
-            app.UseFacebookAuthentication(fb);
-
-            var twitter = new TwitterAuthenticationOptions
-            {
-                AuthenticationType = "Twitter",
-                SignInAsAuthenticationType = signInAsType,
-                ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
-                ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
-            };
-            app.UseTwitterAuthentication(twitter);
-        }
+    
     }
 }
